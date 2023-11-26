9fb2c37396214e846add74a604e53cde791dce72
Cooler Typ99 <david.frings@outlook.de>
2023-11-26 21:05:36 +0100

    Fix

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,23 +6,31 @@ sudo apt-get upgrade -y
 mkdir ~/website/
 cd ~/website/
 
-#sudo apt-get install -y python3
-#sudo apt-get install -y npm
-#sudo apt-get install -y git
-#sudo snap install node --classic
+sudo apt-get install -y python3
+sudo apt-get install -y npm
+sudo apt-get install -y git
+sudo apt-get install -y curl
+sudo snap install node --channel=18/stable --classic
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
+#reboot
+nvm ls
+nvm install --lts
+nvm ls
+
+npm cache clean --force
 
 git clone https://github.com/PaRaMeRoS/Website-Download.git ~/website/
 
 mkdir ~/website/db/
 
-#wget https://repo.mongodb.org/apt/ubuntu/dists/jammy/mongodb-org/7.0/multiverse/binary-amd64/mongodb-org-server_7.0.3_amd64.deb
-#sudo apt install -y ~/website/mongodb-org-server_7.0.3_amd64.deb
+wget https://repo.mongodb.org/apt/ubuntu/dists/jammy/mongodb-org/7.0/multiverse/binary-amd64/mongodb-org-server_7.0.3_amd64.deb
+sudo apt install -y ~/website/mongodb-org-server_7.0.3_amd64.deb
 
 sudo apt-get update
 sudo apt-get upgrade -y
 
 chmod u+x ~/website/scripts/install.sh
-chmod u+x ~/website/scripts/copy.sh
 chmod u+x ~/website/scripts/update.sh
 ~/website/scripts/update.sh
 
