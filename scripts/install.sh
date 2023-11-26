b48c47525d9b9ab35be3adae9eeccd6a8540cb60
Cooler Typ99 <david.frings@outlook.de>
2023-11-26 19:55:54 +0100

    Test

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,17 +6,17 @@ sudo apt-get upgrade -y
 mkdir ~/website/
 cd ~/website/
 
-sudo apt-get install -y python3
-sudo apt-get install -y npm
-sudo apt-get install -y git
-sudo snap install node --classic
+#sudo apt-get install -y python3
+#sudo apt-get install -y npm
+#sudo apt-get install -y git
+#sudo snap install node --classic
 
 git clone https://github.com/PaRaMeRoS/Website-Download.git ~/website/
 
 mkdir ~/website/db/
 
-wget https://repo.mongodb.org/apt/ubuntu/dists/jammy/mongodb-org/7.0/multiverse/binary-amd64/mongodb-org-server_7.0.3_amd64.deb
-sudo apt install -y ~/website/mongodb-org-server_7.0.3_amd64.deb
+#wget https://repo.mongodb.org/apt/ubuntu/dists/jammy/mongodb-org/7.0/multiverse/binary-amd64/mongodb-org-server_7.0.3_amd64.deb
+#sudo apt install -y ~/website/mongodb-org-server_7.0.3_amd64.deb
 
 sudo apt-get update
 sudo apt-get upgrade -y
--- scripts/update.sh
+++ scripts/update.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 cd ~/website/create-users
-sudo npm install -y
+npm install -y
 
 cd ~/website/backend
-sudo npm install -y
+npm install -y
 
 cd ~/website/frontend
-sudo npm install -y
+npm install -y
 
 cd ~/website
-sudo npm install -y
+npm install -y
