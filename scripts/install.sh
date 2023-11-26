c9a1a78446afed1d4dcddf74cd75701469541061
Cooler Typ99 <david.frings@outlook.de>
2023-11-26 14:53:59 +0100

    fix

--- scripts/install.sh
+++ scripts/install.sh
@@ -17,13 +17,14 @@ mkdir ~/website/db/
 wget https://repo.mongodb.org/apt/ubuntu/dists/jammy/mongodb-org/7.0/multiverse/binary-amd64/mongodb-org-server_7.0.3_amd64.deb
 sudo apt install -y ~/website/mongodb-org-server_7.0.3_amd64.deb
 
+chmod u+x ~/website/scripts/install.sh
+chmod u+x ~/website/scripts/update.sh
+chmod u+x ~/website/scripts/nodejs.sh
 ~/website/scripts/nodejs.sh
 
 sudo apt-get update
 sudo apt-get upgrade -y
 
-chmod u+x ~/website/scripts/install.sh
-chmod u+x ~/website/scripts/update.sh
 ~/website/scripts/update.sh
 
 #npm run start (mongod --port 27017 --dbpath ~/website/db --bind_ip 127.0.0.1 --auth
