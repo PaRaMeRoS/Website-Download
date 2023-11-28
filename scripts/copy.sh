9f3798c295a4d910d501c6047fd732d3d5d2c858
Cooler Typ99 <david.frings@outlook.de>
2023-11-28 06:39:47 +0100

    Updated

--- scripts/copy.sh
+++ scripts/copy.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-cd /home/david/
+cd ~
 
 cp /mnt/d/PaRaMeRoS/website-linux /mnt/d/PaRaMeRoS/linux/ -r
 sudo mv /mnt/d/PaRaMeRoS/linux/website-linux /mnt/d/PaRaMeRoS/linux/website -f
-cp /mnt/d/PaRaMeRoS/linux/website /home/david/ -r -f
+cp /mnt/d/PaRaMeRoS/linux/website ~ -r -f
 
 rm /mnt/d/PaRaMeRoS/linux/website -r -f -d
+
+cd ~/website
+npm run start
